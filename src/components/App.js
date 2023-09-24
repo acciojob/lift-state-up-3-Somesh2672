720d26debabdcd30ae0a8ba1827bae2930b976a4
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-09-24 22:22:42 +0530

    new commite

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,57 @@
 
-import React from "react";
-import './../styles/App.css';
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from "react";
+import "./../styles/App.css";
+
+const Parent = () => {
+  const [selectedOption, setSelectedOption] = useState(""); // State in the parent component
+
+  // Function to update selectedOption when a button is clicked
+  const handleButtonClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="parent">
+      <h2>Parent Component</h2>
+      <p>Selected Option: {selectedOption}</p>
+
+      {/* Two Child components with buttons */}
+      <Child option="Option 1" onClick={handleButtonClick} />
+      <Child option="Option 2" onClick={handleButtonClick} />
+    </div>
+  );
+};
+
+const Child = ({ option, onClick }) => {
+  return (
+    <div className="child">
+      <h3>Child Component</h3>
+      <button onClick={() => onClick(option)}>{option}</button>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+      <Parent />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
